4dbc70858f112f85c6e4d18a4bf31246f86cbba9
howznguyen <duyntp2000@gmail.com>
2022-07-10 16:26:36 +0700

    Update BTTH 10

--- Lesson 10/index.py
+++ Lesson 10/index.py
@@ -1,3 +1,11 @@
 import pandas as pd
 df = pd.read_csv('data1.csv')
-print(df.iloc[0:5])
+df["DiemToan"] = pd.to_numeric(df["DiemToan"])
+df["DiemLy"] = pd.to_numeric(df["DiemLy"])
+df["DiemHoa"] = pd.to_numeric(df["DiemHoa"])
+
+df["DiemTrungBinh"] = (df["DiemToan"] + df["DiemLy"] + df["DiemHoa"]) / 3
+
+
+df.to_csv("data_new.csv")
+print(df)
